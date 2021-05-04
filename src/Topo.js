921cee479a5332e320836476561873ff7270e1dc
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-04 18:18:02 -0300

    transformando o topo em JS e modificando alguns estilos em js

---
+++ src/Topo.js
@@ -0,0 +1,8 @@
+export default function Topo(){
+    return (
+        <div class="topo">
+            <p>FoodCamp</p>
+            <p>Sua comida em 6 minutos</p>
+        </div>
+    );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+import Topo from "./Topo";
+
+function App() {
+    return (
+        <>
+            <Topo/>
+        </>
+    );
+}
+
+//ReactDOM.render(<App/>, document.querySelector(".root"));
