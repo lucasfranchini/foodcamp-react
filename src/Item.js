913ffa30fc8274bde6354be8ce8d6df458cb91b6
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-04 21:10:08 -0300

    corrigindo estilos do layout e reajustando a funcao dos precos

--- src/Item.js
+++ src/Item.js
@@ -5,13 +5,11 @@ export default function Item(props){
     const [check,setCheck] = React.useState(false);
     const [price,setPrice] = React.useState(props.price);
     const [type,setType] = React.useState(props.type)
-    function marcar(){
-        setCheck(true);
-        CalculatePrice(price,type);
-    }
-
     return (
-        <li className={check===true ? "item checked": "item"} onClick={marcar}>
+        <li className={check===true ? "item checked": "item"} onClick={()=>{
+            setCheck(true);
+            CalculatePrice(price,type);
+        }}>
             <img src={props.img}alt=""/>
             <p><strong>{props.name}</strong></p>
             <p className="extras">{props.description}</p>
