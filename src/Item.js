c007978d07337cd620c85d2af7cb01c38b915554
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-05 11:18:40 -0300

    adicionando o contador de pratos

--- src/Item.js
+++ src/Item.js
@@ -3,17 +3,22 @@ import CalculatePrice from "./CalculatePrice";
 export default function Item(props){
         
     const [check,setCheck] = React.useState(false);
+    const [counter,setCounter] = React.useState(1);
     const [item] = React.useState(props.item);
     return (
         <li className={check===true ? "item checked": "item"} onClick={()=>{
             setCheck(true);
-            CalculatePrice(item);
+            CalculatePrice(item,counter);
         }}>
             <img src={item.img}alt=""/>
             <p><strong>{item.name}</strong></p>
             <p className="extras">{item.description}</p>
             <p> R$ {item.price}</p>
-            <ion-icon name="checkmark-circle"></ion-icon>
+            <div className= {check===true ? "quantidade mostrar": "quantidade"}>
+                <ion-icon name="remove-outline" style={{color: "#CF2B2B"}} onClick={()=>setCounter(counter-1)}></ion-icon>
+                <span>{counter}</span>
+                <ion-icon name="add-outline" style={{color: "#32B72F"}} onClick={()=>setCounter(counter+1)}></ion-icon>   
+            </div>
         </li>
     );
 }
