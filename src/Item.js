34fa24e9c0a7d0afee74bde1441de544d5f6b2fc
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-05 13:35:14 -0300

    tratando o caso do contador chegar a zero e comecando uma funcao para o contador

--- src/Item.js
+++ src/Item.js
@@ -5,17 +5,22 @@ export default function Item(props){
     const [check,setCheck] = React.useState(false);
     const [counter,setCounter] = React.useState(1);
     const [item] = React.useState(props.item);
+    function changeCounter(){
+        
+    }
     return (
         <li className={check===true ? "item checked": "item"} onClick={()=>{
-            setCheck(true);
-            CalculatePrice(item,counter);
+            if(!check){
+                setCheck(true);
+                CalculatePrice(item,1);
+            }
         }}>
             <img src={item.img}alt=""/>
             <p><strong>{item.name}</strong></p>
             <p className="extras">{item.description}</p>
             <p> R$ {item.price}</p>
             <div className= {check===true ? "quantidade mostrar": "quantidade"}>
-                <ion-icon name="remove-outline" style={{color: "#CF2B2B"}} onClick={()=>setCounter(counter-1)}></ion-icon>
+                <ion-icon name="remove-outline" style={{color: "#CF2B2B"}} onClick={()=>(counter-1===0) ? setCheck(false):setCounter(counter-1)}></ion-icon>
                 <span>{counter}</span>
                 <ion-icon name="add-outline" style={{color: "#32B72F"}} onClick={()=>setCounter(counter+1)}></ion-icon>   
             </div>
