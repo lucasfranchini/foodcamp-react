ab5f5e257a11e9bec47279a864b67cd7d01b1507
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-06 18:48:10 -0300

    mudando a finalizacao de pedido e instalnado react-router-dom

--- src/components/cabecalho e rodape/FinalizarCompra.js
+++ src/components/cabecalho e rodape/FinalizarCompra.js
@@ -3,7 +3,9 @@ export default function FinalizarCompra(props){
     console.log(props.selectedTypes);
     let texto;
     const confirmation = (principal===true && drink===true && desert===true);
-    (confirmation) ? texto = (<> Finalizar pedido</>): texto=(<>Selecione os 3 itens <br/> para fechar o pedido</>);
+    if(confirmation){
+        texto = (<> Finalizar pedido</>)
+    }else texto=(<>Selecione os 3 itens <br/> para fechar o pedido</>);
       return (
             <div className="final">
                 <button className={confirmation ?"fechar-pedido":undefined}> {texto} </button>
