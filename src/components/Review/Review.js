8d7331309ce15d3e644f548536466f3d44929a5f
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-07 12:05:47 -0300

    omplementando botoes no review

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,14 +1,16 @@
 import Topo from "../cabecalho e rodape/Topo";
 import Orders from "../Orders";
 import "./Review.css";
-export default function Review(){
+import {Link} from "react-router-dom";
+export default function Review(props){
+    const {setSelectedItems} = props;
     return(
-        <>
+        <div className="review">
         <Topo/>
         <h1>Revise seu pedido</h1>
         <Orders/>
-        <button></button>
-        <button></button>
-        </>
+        <button className="confirm">Tudo certo, pode pedir!</button>
+        <button className="cancel" onClick={()=>setSelectedItems([])}><Link to="/">Cancelar</Link></button>
+        </div>
     );
 }
