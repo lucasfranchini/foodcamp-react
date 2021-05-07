7ff6b141523ea6f7923ff8aaf2ecea341e8543ec
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-07 16:28:45 -0300

    mudando chamada de funcao de lugar

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,16 +1,14 @@
 import "./Orders.css";
 import PrintItem from "../PrintItem";
-import CalculatePrice from "../CalculatePrice";
-export default function Orders(props){
-    const total ={name:"TOTAL",price: 0};
-    props.selectedItems.forEach(item => CalculatePrice(item,total));
-    total.price = total.price.toFixed(2);
-    total.price =total.price.replace(".",",");
+
+export default function Orders(props){  
+    props.total.price = props.total.price.toFixed(2);
+    props.total.price =props.total.price.replace(".",",");
     
     return(
         <div className="orders">
             {props.selectedItems.map((item,i)=>(<PrintItem key={i} item={item} type="item-selecionado"/> ))}
-            <PrintItem item={total} type="item-selecionado total"/>
+            <PrintItem item={props.total} type="item-selecionado total"/>
         </div>
 
     );
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,17 +1,20 @@
+import "./Review.css";
 import Topo from "../cabecalho e rodape/Topo";
 import Orders from "../Orders/Orders";
-import "./Review.css";
+import CalculatePrice from "../CalculatePrice";
 import {Link} from "react-router-dom";
 import organizeItems from "../OrganizeItems";
+
 export default function Review(props){
     const {setSelectedItems,selectedItems} = props;
     selectedItems.sort(organizeItems);
-    console.log(selectedItems);
+    const total ={name:"TOTAL",price: 0};
+    selectedItems.forEach(item => CalculatePrice(item,total));
     return(
         <div className="review">
         <Topo/>
         <h1>Revise seu pedido</h1>
-        <Orders selectedItems={selectedItems}/>
+        <Orders selectedItems={selectedItems} total={total}/>
         <button className="confirm">Tudo certo, pode pedir!</button>
         <button className="cancel" onClick={()=>setSelectedItems([])}><Link to="/">Cancelar</Link></button>
         </div>
