e1c717c05882fdf0d748b7fd3c5efe90d7944cd3
lucasfranchini <lucasfranchini.a@gmail.com>
2021-05-06 21:17:07 -0300

    iniciando layout da pag de revisar pedido

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,8 +1,14 @@
 import Topo from "../cabecalho e rodape/Topo";
+import Orders from "../Orders";
+import "./Review.css";
 export default function Review(){
     return(
         <>
         <Topo/>
+        <h1>Revise seu pedido</h1>
+        <Orders/>
+        <button></button>
+        <button></button>
         </>
     );
 }
