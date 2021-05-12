0114deb046d59c6ab81cd22b83d7fd0c1f3dae65
thiagoribb <thiribeiro142@gmail.com>
2021-05-12 15:37:04 -0300

    Preparando review

--- src/components/Review/CalculatePrice.js
+++
@@ -1,8 +0,0 @@
-export default function CalculatePrice(item, total){   
-        item.price =item.price.replace(",",".");
-        item.price = parseFloat(item.price);
-        item.price = item.price * item.qtd;
-        total.price += item.price;
-        item.price = item.price.toFixed(2);
-        item.price =item.price.replace(".",",");
-}
--- src/components/Review/OrganizeItems.js
+++
@@ -1,5 +0,0 @@
-export default function organizeItems(a,b){
-    if(a.type===b.type) return 0;
-    if(a.type==="principal" || (a.type==="drink" && b.type==="desert")) return -1;
-    if(b.type==="principal" || (b.type==="drink" && a.type==="desert")) return 1;
-}
